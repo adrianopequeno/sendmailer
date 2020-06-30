f01f7d036dc9f0df8544bca1c7144b30487fa7e8
Adriano Pequeno <adrian.pekeno@gmail.com>
2020-06-30 16:32:18 -0300

    Projeto envio de e-mail de Boas Vindas.

---
+++ README.md
@@ -0,0 +1,9 @@
+Projeto utilizando:
+    body-parser: "^1.19.0",
+    ejs: "^3.1.3",
+    express: "^4.17.1",
+    nodemailer: "^6.4.10",
+    nodemailer-sendgrid-transport: "^0.2.0",
+    nodemon: "^2.0.4"
+Para fins de Estudo de como enviar e-mail de boas vindas utilizando
+o serviço de e-mail SendGrid.
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth:{
+      api_key: "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",
+    }
+}));
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res, next) => {
+  res.render("login", {
+    pageTitle: "Login",
+  })
+})
+
+app.post("/login", (req, res, next) => {
+  console.log(req.body.email);
+  console.log(req.body.password);
+  console.log(req.body.confirmPassword);
+  
+  res.redirect("/");
+
+  // envio de e-mail de boas vindas
+  return transporter.sendMail({
+    to: req.body.email,
+    from: "contato@projectaweb.com.br",
+    subject: "Login feito com sucesso!",
+    html: "<h1>Você realizou o login com sucesso!</h1>",
+  })
+
+})
+
+app.listen(8000, () => {
+  console.log("Sever Started!")
+})
